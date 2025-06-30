3e9aa90192c74f1aaaec07a27a662a0fd2c3c97e
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-06-30 20:11:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
-}
-module.exports = palindrome
+	let left=0,right=str.length-1;
+	while(left<right){
+		if(str[left]!=str[right]){
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
+  }
+const str = prompt('enter a string');  
+	alert(palindrome(str));
+ module.exports = palindrome
